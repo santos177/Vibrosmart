006b3ea9eac95b76d5822a35ebbbdcd1de926018
santos177 <santos177@gmail.com>
2017-09-25 15:36:26 -0300

    se agrega la clase Sensor

--- GSM.py
+++ GSM.py
@@ -6,15 +6,15 @@ import RPi.GPIO as RPIO
 from math import *
 #seteamos el puerto GPIO numero 18
 RPIO.setmode(RPIO.BCM)
-RPIO.setwarnings(False) 
+RPIO.setwarnings(False)
 RPIO.setup(18,RPIO.OUT,initial=RPIO.LOW)
 RPIO.setup(23,RPIO.OUT,initial=RPIO.LOW)
 
 class GSM:
-    
+
     def __init__(self):
         self.data = []
-    
+
     def set(self):
         time.sleep(3)
         RPIO.output(23,RPIO.HIGH)
@@ -28,13 +28,13 @@ class GSM:
         print("Iniciando modulo GSM...")
         port = serial.Serial('/dev/ttyS0',115200, timeout=10)
         print (port.name)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT\r\n')  # Iniciamos la comunicacion
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
-    
+
 
     def off(self):
        print("Apagando el Modulo GSM...")
@@ -46,24 +46,24 @@ class GSM:
 
 
     def call(self,number):
-       self.numero = numero 
+       self.numero = numero
        port = serial.Serial('/dev/ttyS0',115200, timeout=5)
        print (port.name)
        port.write(b'AT+CPIN?\r')#  preguntamos por el PIN
        time.sleep(0.5)
-       respuesta = port.readlines() 
+       respuesta = port.readlines()
        print(respuesta)
        port.write(b'AT+CREG=?\r')
        time.sleep(0.5)
-       respuesta = port.readlines() 
+       respuesta = port.readlines()
        print(respuesta)
        port.write(b'AT+CREG?\r')
        time.sleep(0.5)
-       respuesta = port.readlines() 
+       respuesta = port.readlines()
        print(respuesta)
        port.write(b'ATD+'+number+';\r')# hacemos una llamada!
        time.sleep(10)
-       respuesta = port.readlines() 
+       respuesta = port.readlines()
        print(respuesta)
        port.write(b'ATH\r')
        respuesta = port.readlines()
@@ -73,98 +73,98 @@ class GSM:
 
     def sms(self,number,mensaje):
         self.mensaje = mensaje
-        self.number = number 
+        self.number = number
         port = serial.Serial('/dev/ttyS0',115200, timeout=5)
         print (port.name)
         port.write(b'AT+CPIN?\r\n')#  preguntamos por el PIN
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CMGF=?\r\n')# preguntamos por los modos de texto que soporta el GSM
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CMGF=1\r\n')# seteamos el modo de texto
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CSCS="GSM"\r\n')# seteamos el set de caracteres a usar
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
-        port.write(b'AT+CMGS="+'+number+b'"\r\n')# seteamos el destinatario 
+        port.write(b'AT+CMGS="+'+number+b'"\r\n')# seteamos el destinatario
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(mensaje+b'\r\n')# escribimos el mensaje
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write("\x1A") # activado para enviar mensajes (codigo ASCII numero 26)
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
 
-        
+
     def post(self,datos):
         self.datos = datos
         port = serial.Serial('/dev/ttyS0',115200, timeout=5)
         print (port.name)
         port.write(b'AT+COPS?\r\n')#  preguntamos por las redes presentes
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CGATT=?\r\n')#  preguntamos si el GPRS esta adjunto a la red
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CGATT=1\r\n')#  nos adjuntamos de todas formas
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CGATT=?\r\n')#  preguntamos si el GPRS esta adjunto a la red
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         port.write(b'AT+CIPSTATUS\r\n')# el status de la IP
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CGDCONT=1,"IP","imovil.entelpcs.cl"\r\n')#  ponemos las credenciales que necesitamos para ingresar (EL PDP context y el apn )
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CIPSTATUS\r\n')# el status de la IP
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CGACT=1,1\r\n')#  activamos el PDP context!
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CIPSTATUS\r\n')# el status de la IP
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CIFSR\r\n')# pedimos la IP
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CIPSTATUS\r\n')# el status de la IP
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CIPSTART="TCP","186.64.118.130",80\r\n')#  Nos conectamos a chilealoha!!!!
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CIPSEND\r\n')# Enviando datos a chilealoha!!!!
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'POST /ebox/raspberry.php HTTP/1.1\r\n')# escribimos el mensaje
         time.sleep(0.5)
         respuesta = port.readlines()
-        print(respuesta) 
+        print(respuesta)
         port.write(b'HOST: chilealoha.cl\r\n')
         time.sleep(0.5)
         respuesta = port.readlines()
@@ -191,24 +191,13 @@ class GSM:
         print(respuesta)
         port.write("\x1A") # activado para enviar mensajes (codigo ASCII numero 26)
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CIPCLOSE\r\n')# cerramos la conexion con chilealoha
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
         port.write(b'AT+CIPSHUT\r\n')# cerrando conexion wireless
         time.sleep(0.5)
-        respuesta = port.readlines() 
+        respuesta = port.readlines()
         print(respuesta)
-
-
-numero = "56990250523"
-mensaje ="Esta es solo una prueba Diegote"
-datos = "Muse" 
-gsm = GSM()
-gsm.set()
-gsm.post(datos)
-gsm.off()
-
-
---
+++ Sensor.py
@@ -0,0 +1,96 @@
+#!usr/bin/python
+#Clase I2C para Vibrosmart!
+
+from smbus import SMBus
+import time
+from math import *
+import numpy as np
+import pylab as pl
+import timeit
+
+class Sensor:
+
+    def __init__(self):
+        self.data = []
+
+    def read(self,points,axis):       # points: numero de muestras, axis: eje "x","y "o "z".
+        self.points = points
+        self.axis = axis
+        bus = SMBus(1)
+        bus.write_byte_data(0x1D, 0x2A, 0x00)
+        time.sleep(0.2)
+        bus.write_byte_data(0x1D, 0x2A, 0x01)
+        time.sleep(0.2)
+        bus.write_byte_data(0x1D, 0x0E, 0x00)
+        time.sleep(0.5)
+        t = []
+        x1 = []
+        x2 = []
+        y1 = []
+        y2 = []
+        z1 = []
+        z2 = []
+        c1 = []
+        c2 = []
+        c3 = []
+        data = []
+        strip = []
+        print( "leyendo...")
+        starttime = time.time()
+
+        for i in range(0,points):
+
+              data.append(bus.read_i2c_block_data(0x1D, 0x00,7))
+
+        elapsed = time.time() - starttime
+        print(elapsed)
+
+        for a in range(0,points):
+
+             strip = data[a]
+             x1.append(strip[1])
+             x2.append(strip[2])
+             xAccl = (x1[a]*256 + x2[a]*16) / 16
+
+             if xAccl > 2047 :
+                   xAccl -= 4096
+
+             y1.append(strip[3])
+             y2.append(strip[4])
+             yAccl = (y1[a]*256 + y2[a]*16) / 16
+
+             if yAccl > 2047 :
+                   yAccl -= 4096
+
+             z1.append(strip[5])
+             z2.append(strip[6])
+             zAccl = (z1[a]*256 + z2[a]*16) / 16
+
+             if zAccl > 2047 :
+                   zAccl -= 4096
+
+             t.append(a)
+             c1.append(xAccl)
+             c2.append(yAccl)
+             c3.append(zAccl)
+
+        if axis == "x":
+            return c1
+        elif axis == "y":
+            return c2
+        elif axis == "z":
+            return c3
+        else:
+            print ("Debe ingresar un eje: 'x','y' o 'z'")
+
+    def graph(self,c):    #c: el array con el muestreo
+        self.c = c
+        print ("Graficando...")
+        pl.plot(t,c,'-r')
+        pl.show()
+        bus.write_byte_data(0x1D, 0x2A, 0x00)
+        time.sleep(0.5)
+#pl.plot(t,c1)
+#w = np.array(c2)
+#frec = np.fft.rfft(w,100,-1)     analisis en frecuencia por medio de la FFT
+#print frec
